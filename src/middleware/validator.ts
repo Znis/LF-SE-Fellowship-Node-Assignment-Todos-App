cd4de6e1b8806671d076a7d6897ab92bafe57392
Znis <metwayana@gmail.com>
2024-07-11 10:40:44 +0545

    edit: refactors the HTTP status codes and Custom Error handling

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -1,14 +1,33 @@
 import { NextFunction, Request, Response } from "express";
-import {Schema} from "joi";
+import { func, Schema } from "joi";
 import { BadRequestError } from "../../error/badRequestError";
 
-export function validateReqQuery(schema: Schema){
-    return (req: Request, res: Response, next: NextFunction) => {
-        const { error, value } = schema.validate(req.query);
-        if(error){
-            next(new BadRequestError(error.message));
-        }
-        req.query = value;
-        next();
-    }
+export function validateReqQuery(schema: Schema) {
+     return (req: Request, res: Response, next: NextFunction) => {
+          const { error, value } = schema.validate(req.query);
+
+          if (error) {
+               next(new BadRequestError(error.message));
+               return;
+          }
+
+          req.query = value;
+
+          next();
+     };
+}
+
+export function validateReqBody(schema: Schema) {
+     return (req: Request, res: Response, next: NextFunction) => {
+          const { error, value } = schema.validate(req.body);
+
+          if (error) {
+               next(new BadRequestError(error.message));
+               return;
+          }
+
+          req.body = value;
+
+          next();
+     };
 }
--- src/schema/user.ts
+++ src/schema/user.ts
@@ -16,3 +16,38 @@ export const getUserQuerySchema = joi.object({
    }).options({
         stripUnknown: true,
    });
+
+   export const createUserBodySchema = joi.object({
+        name: joi.string().required().messages({
+             "any.required": "Name is required",
+        }),
+   
+        email: joi.string().email().required().messages({
+             "any.required": "Email is required",
+             "string.email": "Email must be valid format",
+        }),
+   
+        password: joi.string().required().min(8).messages({
+             "any.required": "password is required",
+             "string.min": "Password must be at least 8 characters",
+             "password.uppercase": "Password must have at least one uppercase character",
+             "password.lowercase": "Password must have at least one lowercase character",
+             "password.special": "Password must have at least one special character",
+        })
+        .custom((value, helpers) => {
+                if (!/[A-Z]/.test(value)) {
+                     return helpers.error("password.uppercase");
+                }
+                if (!/[a-z]/.test(value)) {
+                     return helpers.error("password.lowercase");
+                }
+                if (!/[!@#$%^&*()_+]/.test(value)) {
+                     return helpers.error("password.special");
+                }
+ 
+                return value;
+           }).options({
+                stripUnknown: true,
+           }),
+
+   });
